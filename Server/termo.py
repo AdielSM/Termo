86cc631916a4c8c3c9f8d4785600e2576bbb3d73
AdielSM <129189553+AdielSM@users.noreply.github.com>
2023-12-06 15:08:28 -0300

    Refatoração de checkWord + documentação

--- Server/termo.py
+++ Server/termo.py
@@ -2,7 +2,7 @@ from typing import List, Dict, Union
 from enum import Enum
 
 from .AVLtree import AVLtree
-from utils import PilhaSequencial
+from utils import PilhaSequencial, sumario_protocolo
 from .palavrasRepository import carregarPalavras
 
 class Estado(Enum):
@@ -16,6 +16,7 @@ class Estado(Enum):
 
 class Termo:
     bancoPalavras: AVLtree = AVLtree()
+    protocolo = sumario_protocolo()
     
     palavras = carregarPalavras()  # Carrega as palavras do palavrasRepository
     bancoPalavras.add_elements(palavras)
@@ -29,6 +30,15 @@ class Termo:
         self.iniciarJogo(qtdTentativas)
 
     def iniciarJogo(self, qtdTentativas: int) -> None:
+        """
+        Inicia o jogo com a quantidade de tentativas especificada.
+
+        Args:
+            qtdTentativas (int): A quantidade de tentativas disponíveis.
+
+        Returns:
+            None
+        """
         self.__palavra: str = self.__escolherPalavraAleatoria()
         self.__dictPalavra: Dict[str, List[int]] = self.__criarDictPalavra(self.__palavra)
         self.__qtdTentativasRestantes: int = qtdTentativas
@@ -54,6 +64,15 @@ class Termo:
         return palavra
 
     def __criarDictPalavra(self, palavra: str) -> Dict[str, List[int]]:
+        """
+        Cria um dicionário onde as chaves são as letras da palavra e os valores são as posições em que as letras aparecem na palavra.
+
+        Args:
+            palavra (str): A palavra para criar o dicionário.
+
+        Returns:
+            Dict[str, List[int]]: O dicionário onde as chaves são as letras da palavra e os valores são as posições em que as letras aparecem na palavra.
+        """
         dict_palavra: Dict[str, List[int]] = {}
         for i, letra in enumerate(palavra):
             if letra not in dict_palavra:
@@ -62,23 +81,55 @@ class Termo:
                 dict_palavra[letra].append(i)
         return dict_palavra
 
-    def checkWord(self, palavra: str) -> int | List[int]:
-        # Só avança com tentativa
-        if not self.__estadoDoJogo == Estado.Jogo_com_tentativa: pass
+    def checkWord(self, palavra: str) -> Union[int, List[int]]:
+        """
+        Verifica se a palavra fornecida é válida de acordo com as regras do jogo.
 
+        Args:
+            palavra (str): A palavra a ser verificada.
+
+        Returns:
+            Union[int, List[int]]: O feedback da palavra ou um código de erro.
+
+        Raises:
+            ValueError: Se o jogo não estiver no estado de tentativa.
+
+        """
+        # Verifica se o estado do jogo permite uma tentativa
+        if self.__estadoDoJogo != Estado.Jogo_com_tentativa:
+            raise ValueError("O jogo não está no estado de tentativa")
+
+        # Verifica se a palavra é igual à palavra do jogo
         if palavra == self.__palavra:
             self.__estadoDoJogo = Estado.Vitoria
-            return 202
-        
+            return Termo.protocolo["PALAVRA_CORRETA"]
+
+        # Verifica se a palavra tem o mesmo tamanho que a palavra do jogo
         elif len(palavra) != len(self.__palavra):
-            return 403
-        
+            return Termo.protocolo["TAMANHO_INCORRETO"]
+
+        # Verifica se a palavra está presente no banco de palavras
         elif not Termo.bancoPalavras.is_present(palavra):
-            return 404
-        
+            return Termo.protocolo["PALAVRA_INEXISTENTE"]
+
+        # Verifica se a palavra já foi tentada antes
         elif self.__pilhaPalavras.verifica_elemento(palavra):
-            return 405
-        
+            return Termo.protocolo["PALAVRA_REPETIDA"]
+
+        # Gera o feedback para a palavra
+        feedback = self.__generateFeedback(palavra)
+
+        self.__qtdTentativasRestantes -= 1
+        self.__pilhaPalavras.empilha(palavra)
+
+        # Verifica se o jogo ainda tem tentativas restantes
+        if self.__qtdTentativasRestantes == 0:
+            self.__estadoDoJogo = Estado.Jogo_sem_tentativa
+
+        return feedback
+
+    def __generateFeedback(self, palavra: str) -> List[int]:
+        """Gera o feedback para uma palavra com base na palavra do jogo."""
         feedback: List[int] = []
         for index, letra in enumerate(palavra):
             if letra in self.__dictPalavra and index in self.__dictPalavra[letra]:
@@ -87,13 +138,5 @@ class Termo:
                 feedback.append(1)
             else:
                 feedback.append(0)
-
-        self.__qtdTentativasRestantes -= 1
-        self.__pilhaPalavras.empilha(palavra)
-
-        if self.__qtdTentativasRestantes == 0:
-            self.__estadoDoJogo = Estado.Jogo_sem_tentativa
-        
         return feedback
-        
     
