62c80601c42fe3689b8fbf7d9469abda017acd9e
AdielSM <129189553+AdielSM@users.noreply.github.com>
2023-12-09 21:23:23 -0300

    Separando os loaders das palavras + padronização em inglês

---
+++ Server/termo_words_loader.py
@@ -0,0 +1,12 @@
+TERMO_WORD_BANK_URL = 'words_bank/bdTermo.txt'
+
+def load_termo_words() -> list:
+    """
+    Carrega as palavras do arquivo e retorna uma lista de palavras.
+
+    Returns:
+        list: Uma lista de palavras carregadas do arquivo.
+    """
+    with open(TERMO_WORD_BANK_URL, "r", encoding="utf-8") as file:
+        words = [word.strip().replace('\x00', '') for word in file.readlines()]
+        return words
---
+++ Server/words_loader.py
@@ -0,0 +1,12 @@
+WORD_BANK_URL = 'words_bank/bd.txt'
+
+def load_words() -> list:
+    """
+    Carrega as palavras do arquivo e retorna uma lista de palavras.
+
+    Returns:
+        list: Uma lista de palavras carregadas do arquivo.
+    """
+    with open(WORD_BANK_URL, "r", encoding="utf-8") as file:
+        words = [word.strip().replace('\x00', '') for word in file.readlines()]
+        return words
